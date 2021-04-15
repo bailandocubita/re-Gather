a0806271203fb814afe50328d47b0dcf3ee6ee3a
Kayla Thomas <kaylathomas@Kaylas-MacBook-Air-2.local>
2021-04-14 20:48:43 -0500

    updated array

--- src/app/bucket-list-page/bucket-list-page.component.ts
+++ src/app/bucket-list-page/bucket-list-page.component.ts
@@ -15,33 +15,24 @@ export class BucketListPageComponent implements OnInit {
 bucketEvents: Event [] = [
   {
     title: 'Music Festivle',
-    description: 'Music festivle for all ages',
     location:'Chicago',
     date: 'April 27th 2021', 
     category: 'Music', 
-    price: 200,
-    tickets: 'www.google.com/music',
-    accessibility: 'facility is not wheelchair accesbile'
+    price: 200
   },
   {
     title: 'Air Show',
-    description: 'public aviation show where exhibitted aircaft demonstrate aerobactic demonstrations.',
     location:'Chicago',
     date: 'May 5th 2021', 
     category: 'Aviation', 
-    price: 10,
-    tickets: 'www.google.com/aviation',
-    accessibility: 'wheelchair accesbile'
+    price: 10
   },
   {
     title: 'Art Gallery',
-    description: 'skilled painters, photographers, and artist of all kind came ',
     location:'Chicago',
     date: 'April 27th 2021', 
     category: 'Art', 
-    price: 60,
-    tickets: 'www.google.com/art',
-    accessibility: 'wheelchair accesbile'
+    price: 60
   }
 ]
 }
