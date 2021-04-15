e6376d99cd02cf74161f5b9a53908815b23a1cfb
shauna.s.peterson@hotmail.com <shauna.s.peterson@hotmail.com>
2021-04-15 18:44:35 -0400

    First push

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -17,24 +17,33 @@ export class EventListComponent implements OnInit {
   events: Event[] = [
     {
       title: 'Art Show',
+      description: 'This is an abstract modernism show.',
       location: 'Detroit',
       date: '4-16-2021',
       category: 'art',
-      price: 20
+      price: 20,
+      tickets: 'https://www.google.com/',
+      accessibility: 'wheelchair accessible'
     },
     {
       title: 'Concert',
+      description: 'This is a music concert. Yay!',
       location: 'Cleveland',
       date: '5-6-2021',
       category: 'music',
-      price: 60
+      price: 60,
+      tickets: 'https://www.google.com/',
+      accessibility: 'venue is not wheelchair accessible'
     },
     {
       title: 'Play',
+      description: 'This is going to be a great play to see!',
       location: 'Grand Rapids',
       date: '6-26-2021',
       category: 'theatre',
-      price: 40
+      price: 40,
+      tickets: 'https://www.google.com/',
+      accessibility: 'wheelchair accessible'
     }
   ]
 
