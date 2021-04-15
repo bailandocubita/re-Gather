ab34e2a5ac26f62dfd2d7594437285e58514330e
Kayla Thomas <kaylathomas@Kaylas-MacBook-Air-2.local>
2021-04-14 20:36:19 -0500

    adding Event array

--- src/app/bucket-list-page/bucket-list-page.component.ts
+++ src/app/bucket-list-page/bucket-list-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Event} from '../event'; 
 
 @Component({
   selector: 'app-bucket-list-page',
@@ -11,5 +12,36 @@ export class BucketListPageComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+bucketEvents: Event [] = [
+  {
+    title: 'Music Festivle',
+    description: 'Music festivle for all ages',
+    location:'Chicago',
+    date: 'April 27th 2021', 
+    category: 'Music', 
+    price: 200,
+    tickets: 'www.google.com/music',
+    accessibility: 'facility is not wheelchair accesbile'
+  },
+  {
+    title: 'Air Show',
+    description: 'public aviation show where exhibitted aircaft demonstrate aerobactic demonstrations.',
+    location:'Chicago',
+    date: 'May 5th 2021', 
+    category: 'Aviation', 
+    price: 10,
+    tickets: 'www.google.com/aviation',
+    accessibility: 'wheelchair accesbile'
+  },
+  {
+    title: 'Art Gallery',
+    description: 'skilled painters, photographers, and artist of all kind came ',
+    location:'Chicago',
+    date: 'April 27th 2021', 
+    category: 'Art', 
+    price: 60,
+    tickets: 'www.google.com/art',
+    accessibility: 'wheelchair accesbile'
+  }
+]
 }
