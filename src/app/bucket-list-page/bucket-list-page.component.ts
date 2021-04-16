f0bd9d64a7d43d369625a0d38d3fbb5f0eb19e3e
Kayla Thomas <kaylathomas@Kaylas-MacBook-Air-2.local>
2021-04-15 20:25:14 -0500

    link

--- src/app/bucket-list-page/bucket-list-page.component.ts
+++ src/app/bucket-list-page/bucket-list-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
 import {Event} from '../event'; 
+
 
 @Component({
   selector: 'app-bucket-list-page',
@@ -12,27 +15,5 @@ export class BucketListPageComponent implements OnInit {
 
   ngOnInit(): void {
   }
-bucketEvents: Event [] = [
-  {
-    title: 'Music Festivle',
-    location:'Chicago',
-    date: 'April 27th 2021', 
-    category: 'Music', 
-    price: 200
-  },
-  {
-    title: 'Air Show',
-    location:'Chicago',
-    date: 'May 5th 2021', 
-    category: 'Aviation', 
-    price: 10
-  },
-  {
-    title: 'Art Gallery',
-    location:'Chicago',
-    date: 'April 27th 2021', 
-    category: 'Art', 
-    price: 60
-  }
-]
+
 }
