1e0b9ba19b2de1ac0f5769f4cac95616f143b961
kkholland <katherinebholland@gmail.com>
2021-04-21 21:56:48 -0400

    api connected to detail

--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, SubscribableOrPromise, Subscription } from 'rxjs';
 import { TicketmasterService } from '../ticketmaster.service';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
@@ -12,17 +12,17 @@ import { switchMap } from 'rxjs/operators';
 export class EventDetailsComponent implements OnInit {
   event: Observable<any> | any | null = null;
   p: Event | null = null;
+  subscription: any;
 
   constructor(private service: TicketmasterService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    // this.route.parent.paramMap
-		// 	.pipe(switchMap(p => this.service.getFood(+p.get('id'))))
-		// 	.subscribe(food => this.food = food);
 
-    // this.event = this.service.getEventById('vvG1HZpdeUZdW6');
+    this.subscription = this.route.paramMap.pipe(switchMap(p => this.service.getEventById(p?.get('id')))).subscribe(event => this.event = event);
+  }
 
-    this.route.paramMap.pipe(switchMap(p => this.service.getEventById(p?.get('id')))).subscribe(event => this.event = event);
+  ngOnDestroy():void{
+    this.subscription.unsubscribe();
   }
 
 }
